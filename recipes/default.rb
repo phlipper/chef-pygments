b8c9350ff8fec008d290155657e959fa3ed22867
Phil Cohen <github@phlippers.net>
2012-03-29 20:43:23 -0700

    initial cleanup and commit

---
+++ recipes/default.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: pygments
+# Recipe:: default
+#
+# Copyright 2011-2012, Phil Cohen
+#
+
+# apt-get
+package "python-setuptools"
+
+# easy_install
+easy_install_package "Pygments" do
+  action :install
+end
